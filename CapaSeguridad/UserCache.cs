71d691c8c8682fcc88e3684c7305c6ea7ffc9c69
Yulian2405 <juliangalvez1524@gmail.com>
2025-11-01 05:24:26 -0600

    Creo Login Julian

---
+++ CapaSeguridad/UserCache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaSeguridad
+{
+    internal class UserCache
+    {
+        public static int CodigoUsuario { get; set; }
+        public static int CodigoEmpleado { get; set; }
+        public static string NombreUsuario { get; set; }
+        public static string Contrasenia { get; set; }
+        public static string Rol { get; set; }
+        public static string Estado { get; set; }
+
+
+    }
+}
